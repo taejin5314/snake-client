114743dfdd1943a5c1567d0a80d6860fb1e5fdbe
Taejin Jung <taejin5314@gmail.com>
2021-06-29 19:35:09 +0000

    Client can send their name to the server

--- client.js
+++ client.js
@@ -9,6 +9,8 @@ const connect = function() {
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
+    console.log('Successfully connected to game server');
+    conn.write("Name: TJT");
     conn.on("data", (data) => {
       console.log(data);
     });
