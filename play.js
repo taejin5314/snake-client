37713cf0d69d3b73af66cfc3a4514fe67e932595
Taejin Jung <taejin5314@gmail.com>
2021-06-29 21:00:31 +0000

    create input.js for setupInput function

--- play.js
+++ play.js
@@ -1,24 +1,6 @@
 const { connect } = require('./client');
-
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  
-  const handleUserInput = function(key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
+const {setupInput} = require('./input');
 
 console.log("Connecting ...");
 connect();
 setupInput();
-
